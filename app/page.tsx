b855a2760562a589b9d83df56796160cc48de848
mkbeefcake <mkblockchaindev@outlook.com>
2023-06-07 23:53:11 +0800

    20230607: landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,18 @@
-import Image from 'next/image'
-
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <div className="w-full min-h-screen grid grid-cols-1 content-center">
+      <div className="flex flex-row items-center justify-content">
+          <div className="flex flex-col items-center ml-20 lg:items-start w-full max-w-xl lg:w-1/2 text-center lg:text-left z-30">
+              <h2 className="text-indigo-900 font-black text-2xl sm:text-4xl leading-tight relative mb-4 xl:mb-8">ChatAutomation</h2>
+              <h1 className="text-white-900 font-black text-3xl sm:text-6xl leading-tight relative mb-4 xl:mb-8">Here is the slogan</h1>
+              <p className="text-base sm:text-lg xl:text-xl text-gray-600 mb-8 pr-0 lg:pr-20">Here is the web tool description. Here is the web tool description</p>
+              <a href="/dashboard/login" className="mt-0 sm:mt-1 px-8 py-4 rounded-md fold-bold text-base bg-indigo-600 shadow-xl text-white border-t border-gray-200 w-auto font-bold relative self-start inline-block lg:mx-0 mx-auto">Log in</a>
+          </div>
+          <div className="flex flex-col items-end justify-center h-full w-full lg:w-1/2 ms:pl-10 relative z-50">
+            <img src="homepage.svg"/>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        Home
+          </div>
       </div>
-
-    </main>
+    </div>
   )
 }
